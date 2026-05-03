991a4fb97adccadf3228b7e98ee87d68499ada24
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-03 23:03:34 +0530

    create s3 bucket using terraform

--- Day-03/main.tf
+++ Day-03/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 # create s3 bucket
 resource "aws_s3_bucket" "demo_bucket01" {
-  bucket = "my-tf-test-bucket_raji013ella"
+  bucket = "my-tf-test-bucket-raji013ella"
 
 
   tags = {
