70d84569e677f1f9f51f0acf7767a95b4c9a41d8
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-09 16:19:33 +0530

    state file

---
+++ Day-4/main.tf
@@ -0,0 +1,36 @@
+# Configure the AWS Provider
+terraform {
+    backend "s3" {
+    bucket = "my-tf-test-bucket-raji013ella" 
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    profile = "default"
+    encrypt = true
+    use_lockfile = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# create s3 bucket
+resource "aws_s3_bucket" "demo_bucket01" {
+  bucket = "my-tf-test-bucket-raji013ella"
+
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+
+}
