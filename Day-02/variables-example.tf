5bd9cd4f67b42a89298d9132d8085a2251ff5e8d
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-14 13:10:46 +0530

    variables example

--- Day-02/variables-example.tf
+++ Day-02/variables-example.tf
@@ -9,6 +9,7 @@ variable "instance_type" {
 variable "ami_id" {
     description = "EC2 ami id"
     type = string
+    default = "ami-0dcc1e21636832c5d"
 }
 
 #create an EC2 instance using the input variables
