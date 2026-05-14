5e203c35c4e6f0e525a25ffc4e58c0f72593dcb7
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-14 12:49:28 +0530

    variables example

---
+++ Day-02/variables-example.tf
@@ -0,0 +1,24 @@
+# Define an input variable for the EC2 instance type
+variable "instance_type" {
+    description = "Ec2 instance type"
+    type = string
+    default = "t2.micro"
+}
+
+# Define an input variable for the EC2 instance AMI ID
+variable "ami_id" {
+    description = "EC2 ami id"
+    type = string
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#create an EC2 instance using the input variables
+resource "aws_instance" "example_instance" {
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+ 
+}
