466ff4781dc12ddbaabd0b47c7c5a82a2e69939c
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-15 22:47:13 +0530

    terraform variable example

--- Day-02/variables-example.tf
+++ Day-02/variables-example.tf
@@ -12,9 +12,15 @@ variable "ami_id" {
     default = "ami-056ee4729b02a3d8c"
 }
 
+variable "subnet" {
+    type = string
+    default = "subnet-08be771af4a1c652b"
+}
+
 #create an EC2 instance using the input variables
 resource "aws_instance" "example_instance" {
   ami                     = var.ami_id
   instance_type           = var.instance_type
+  subnet_id               = var.subnet
  
 }
