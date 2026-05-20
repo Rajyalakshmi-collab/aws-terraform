36a5ba414ce36ff5e21220e2b51944baac8397fd
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-20 16:06:35 +0530

    module in tf

---
+++ Day-01/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_type" {
+    description = "Ec2 instance type"
+    type = string
+    default = "t3.micro"
+}
+
+#output variable to display public IP address
+output "public_ip" {
+    value = aws_instance.example_instance.public_ip
+}
+
+# Define an input variable for the EC2 instance AMI ID
+variable "ami_id" {
+    description = "EC2 ami id"
+    type = string
+    default = "ami-056ee4729b02a3d8c"
+}
+
+variable "subnet" {
+    type = string
+    default = "subnet-08be771af4a1c652b"
+}
