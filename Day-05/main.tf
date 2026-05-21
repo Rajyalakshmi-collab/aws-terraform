748b2cd91ef9e5939365c87e0fb7230a27ad5fba
Rajyalakshmi-collab <rajyalakshmipuli2@gmail.com>
2026-05-21 13:24:34 +0530

    file provisioner in terraform

--- Day-05/main.tf
+++ Day-05/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "eu-north-1"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
 }
 
